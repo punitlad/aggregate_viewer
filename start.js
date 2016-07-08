b795e715bbc33e923528434781377598ac88cba9
Punit Lad <punitlad@gmail.com>
2016-07-07 21:24:08 -0400

    added property parsing and argument parsing

--- start.js
+++ start.js
@@ -1,15 +1,23 @@
-console.log("starting up aggregate viewer");
-
 var mongo_client = require('mongodb').MongoClient;
 var assert = require('assert');
+var properties_reader = require('properties-reader');
 
-mongo_client.connect("mongodb://localhost:27017/axontrader", function(err, db) {
+var properties = properties_reader('./settings.properties');
+var aggregate_id_key = properties.get('aggregate.id.key');
+var database_connection = properties.get('database.connection');
+var table = properties.get('database.eventstore.name')
+var aggregate_id = process.argv[2];
+
+mongo_client.connect(database_connection, function(err, db) {
   assert.equal(null, err);
 
-  db.collection('domainevents').find({aggregateIdentifier: '7f8b296b-3f6c-47cf-b1ce-fcf2bd9d3d64'}).toArray(
+  db.collection('domainevents').find({aggregate_id_key: aggregate_id}).toArray(
     function(err, docs) {
-      console.log(docs[0].payloadType);
-
+      if (docs.length != 0) {
+        console.log(docs[0].payloadType);
+      } else {
+        console.log("nothing found for " + aggregate_id + "-T_T-");
+      }
       db.close();
     });
   }
